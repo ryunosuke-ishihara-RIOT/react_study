20a667d7226fc8214289c5c459ba148fae7c792b
ryunosuke-ishihara <sp.ryunosuke1@gmail.com>
2021-01-21 23:20:47 +0900

    Toggle_component完成

--- src/components/core/Toggle.jsx
+++ src/components/core/Toggle.jsx
@@ -1,19 +1,22 @@
-import React,{useState} from "react";
-import styled from "styled-components"
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const Toggle =(props) =>{
-    const [state,setState] = useState(props);
-    const {type,value} = state;
-    const [isOn,isSetOn] = useState(true);
-    const handleChange = () =>setOn(!isOn);
-
-    return (
+const Toggle = (props) => {
+  const [state, setState] = useState(props);
+  const { type, value } = state;
+  const [isOn, setIsetOn] = useState(true);
+  const handleChange = () => setIsetOn(!isOn);
+  return (
     <>
-        <StyledButton type={type} onClick={handleChange}>{value}</StyledButton>
-        {isOn ? <span>ON</span>:<span>OFF</span>}
-    </>)
-}
+      <StyledButton type={type} onClick={handleChange}>
+        {value}
+      </StyledButton>
+      {isOn ? <span>ON</span> : <span>OFF</span>}
+    </>
+  );
+};
 
-const StyledButton = styled.button
-`color:red`;
+const StyledButton = styled.button`
+  color: red;
+`;
 export default Toggle;
