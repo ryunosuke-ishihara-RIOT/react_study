3a8cf638f2095395bfc4988ee7ba912e80ba5166
ryunosuke-ishihara <sp.ryunosuke1@gmail.com>
2021-01-21 22:27:57 +0900

    改行修正

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import Toggle from "./core/Toggle"
+import Button from "./core/Button"
+
 function App() {
   return (<>
-  <Toggle type="button" value="Toggle Button" />
+<<<<<<< Updated upstream
+    <Toggle type="button" value="Toggle Button" />
+=======
+    <Button type="button" value="alert" isDisable={true}/>
+>>>>>>> Stashed changes
   </>)
 
 }
--- src/components/core/Toggle.jsx
+++ src/components/core/Toggle.jsx
@@ -1,18 +1,19 @@
 import React,{useState} from "react";
-import Styled from "styled-components"
+import styled from "styled-components"
 
 const Toggle =(props) =>{
     const [state,setState] = useState(props);
     const {type,value} = state;
-    const [isOn,setOn] = useState(true);
+    const [isOn,isSetOn] = useState(true);
     const handleChange = () =>setOn(!isOn);
 
-    return (<>
-    <StyledButton type={type} onClick={handleChange}>{value}</StyledButton>
-    {isOn ? <span>ON</span>:<span>OFF</span>}
+    return (
+    <>
+        <StyledButton type={type} onClick={handleChange}>{value}</StyledButton>
+        {isOn ? <span>ON</span>:<span>OFF</span>}
     </>)
 }
 
-const StyledButton = Styled.button
+const StyledButton = styled.button
 `color:red`;
 export default Toggle;
