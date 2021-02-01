fec51a2728f234f135820495115b23ff27c296d3
ryunosuke-ishihara <sp.ryunosuke1@gmail.com>
2021-02-02 00:23:26 +0900

    fileuploadコンポーネント完成

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,8 @@
-import Button from "./core/Button";
-import DenselyJoined from "./pages/DenselyJoined";
-import Modal from "./core/Modal";
-import Toggle from "./core/Toggle";
 import { FileUpload } from "./core/FileUpload";
 
 function App() {
   return (
     <>
-      {/* <Toggle type="button" value="Toggle Button" /> */}
-      {/* <Button value="alert" isDisabled={true}/> */}
-      {/* <Modal value="push" text="モーダル" /> */}
-      {/* <DenselyJoined value="push" text="モーダル" /> */}
       <FileUpload />
     </>
   );
